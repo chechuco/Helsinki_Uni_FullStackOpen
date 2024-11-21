0aef2375cb1012fb697657a531b1b4a74daf73df
Josechu Carballedo <josechu.carballedo@gmail.com>
2024-11-21 08:10:36 +0100

    Trabajando ej 1.1

---
+++ Exercises/Part_1/infocourse/src/App.jsx
@@ -0,0 +1,36 @@
+const Header = (props) => {
+  return( <h1>{props.course}</h1>)
+}
+
+const Content = (props) => {
+  return(
+    <>
+      <p>{props.part1}</p>
+      <p>{props.exercise1}</p>
+      <p>{props.part2}</p>
+      <p>{props.exercise2}</p>
+      <p>{props.part3}</p>
+      <p>{props.exercise3}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1}/>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App
