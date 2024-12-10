2c15ee28a030b19b7e1d6ec79c240da1294815de
Josechu Carballedo <josechu.carballedo@gmail.com>
2024-12-10 10:15:25 +0100

    1.d revisiting

--- Practice/part1/src/App.jsx
+++ Practice/part1/src/App.jsx
@@ -3,9 +3,15 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(0)
 
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
+  }
+
   return (
     <div>
-      <button onClick= > reset to zero</button>
+      <button onClick={handleClick}>reset to zero</button>
+      <p>{value}</p>
     </div>
   )
 }
